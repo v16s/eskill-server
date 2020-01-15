c2ee3f903fd03c6a99b0154dffbd54626f56961a
v16s <shankar.vignesh@gmail.com>
2020-01-15 23:22:41 +0530

    moved to typescript

---
+++ server/graphql/resolvers/mutations/index.ts
@@ -0,0 +1,17 @@
+import { global } from './global';
+import { user } from './user';
+import { admin } from './admin';
+import { campus } from './campus';
+import { question } from './question';
+import { student } from './student';
+import { faculty } from './faculty';
+
+export default {
+  ...global,
+  ...user,
+  ...admin,
+  ...campus,
+  ...question,
+  ...student,
+  ...faculty
+};
